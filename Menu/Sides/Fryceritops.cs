6dc03825e97897d85d06ceddcd24210e89a1163f
Nathan Brown <njbrown@ksu.edu>
2019-09-18 15:16:00 -0500

    Fryceritops

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,24 @@
+﻿using DinoDiner.Menu.Sides;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    class Fryceritops : Side
+    {
+        public override List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Potato", "Vegetable Oil", "Salt" };
+            }
+        }
+
+        public Fryceritops()
+        {
+            this.Price = 0.99;
+            this.Calories = 222;
+        }
+    }
+}
